ab14a5a217d9ca733d28e5cb9376ca457a9c7d3a
Yinong Lin <ynlin@mobvoi.com>
2017-07-31 17:55:26 +0800

    Mars man!

---
+++ openfstexamples/bin/makelex.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+import sys
+
+lines = sys.stdin.readlines()
+
+s = 0
+eps = '<epsilon>'
+for line in lines:
+  cols = line.split()
+  if cols[1] == '0':
+    continue  # epsilon
+  word = cols[0]
+  letters = list(word)
+  for i in range(0, len(letters)):
+    if i == 0:
+      print 0, s + 1, letters[i], word
+    else:
+      s = s + 1
+      print s, s + 1, letters[i], eps
+  s = s + 1
+  print s
